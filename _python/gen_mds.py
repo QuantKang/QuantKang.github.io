3bcb237e54dbde8b9fe5ae8cf6e769a369c2def6
QuantKang <jaeho.kang@gmail.com>
2024-01-13 13:17:15 +0900

    test-commit

--- _python/gen_mds.py
+++ _python/gen_mds.py
@@ -13,13 +13,16 @@ def fstr(template):
     return eval(f"f'{template}'").strip()
 
 
-_BASE_PATH = '../_posts/'
+_BASE_PATH = '../'
 _MD_TMPL = '''
 ---
 title: {name}({ticker}) 주가 정보
 categories:
 - Stock
 ---
+
+![{ticker}](/assets/stock_images/{ticker}.png)
+
 {name}는 시가({open_price}원), 고가({high_price}), 저가({low_price}), 종가({close_price})였습니다.
 '''
 _MD_TMPL = _MD_TMPL.replace('\n', '{_NEW_LINE}')
@@ -31,6 +34,9 @@ for ticker in tqdm(tickers):
   name = stock_df[stock_df.Code == ticker].Name.iloc[0]
   df = fdr.DataReader(ticker)
   open_price, high_price, low_price, close_price = df[['Open', 'High', 'Low', 'Close']].iloc[-1]
-  _FILE_PATH = f'{_BASE_PATH}/2024-01-01-{ticker}.md'
+  _FILE_PATH = f'{_BASE_PATH}/_posts/2024-01-01-{ticker}.md'
+  _IMG_PATH = f'{_BASE_PATH}/assets/stock_images/{ticker}.png'
   with open(_FILE_PATH, 'w') as f:
     f.write(fstr(_MD_TMPL))
+  plt.savefig(_IMG_PATH)
+  plt.cla()
